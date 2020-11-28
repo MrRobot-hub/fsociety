afebac9a9adc1b7fc057c827ebb358b3ebcae0a2
Umer Farid <57315483+MrRobot-hub@users.noreply.github.com>
2020-11-28 01:51:24 -0800

    fsociety utilities

--- utilities/utility.py
+++ utilities/utility.py
@@ -11,7 +11,11 @@ class Check_arch:
     def lang_query_curl(self):
         # print(type(self.platfrm))
         if platform.machine().endswith("64"):
-            os.system(f"{self.path_curl64} cht.sh/{self.query_language}/{self.query}")
+            if os.system(f"{self.path_curl64} cht.sh/{self.query_language}/{self.query}"):
+                print("\033[31mMake sure you have a strong internet connection and try again!\033[0m")
+                
+            else:
+                pass
         else:
             os.system(f"{self.path_curl32} cht.sh/{self.query_language}/{self.query}")
         return True
