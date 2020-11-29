cbd8d3e59b1795d0489b45a587f41f776efe0e5b
Umer Farid <57315483+MrRobot-hub@users.noreply.github.com>
2020-11-28 19:23:26 -0800

    Update time.py

--- utilities/time.py
+++ utilities/time.py
@@ -3,11 +3,9 @@ import datetime
 
 def get_time():
     local_time = time.localtime()
-
     current_time = time.strftime("%H-%M-%S", local_time)
-   
-
     return current_time
+
 def get_date():
     date = datetime.date.today()
     today_is = date.strftime("%d-%m-%Y")
