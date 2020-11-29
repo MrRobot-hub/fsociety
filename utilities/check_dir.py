aafd8cd0e7aff76eaa2fc90787954802eb1500d2
Umer Farid <57315483+MrRobot-hub@users.noreply.github.com>
2020-11-29 01:24:00 -0800

    Update check_dir.py

--- utilities/check_dir.py
+++ utilities/check_dir.py
@@ -1,9 +1,4 @@
 import os
 
-
-def create_dir():
-    dir_path = os.getcwd()
-    if not os.path.exists(f"{dir_path}\\etc"):
-        os.system("mkdir etc")
-    if not os.path.exists(f"{dir_path}\\logs"):
-        os.system("mkdir logs")
+def create_dir(dirname):
+    os.system(f"mkdir {dirname}")
